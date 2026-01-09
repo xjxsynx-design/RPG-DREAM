533f88c29940852f4d80bb90b0511d78d81f7a6f
xjxsynx-design <xjxsynx@gmail.com>
2026-01-08 22:10:41 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('rpgwb').then(c=>c.addAll([
-    './','index.html','style.css','app.js','manifest.json'
-  ])));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
