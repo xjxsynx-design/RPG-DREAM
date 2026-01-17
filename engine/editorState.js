4e3a60115af27f330bb999be8aa0353a45345540
xjxsynx-design <xjxsynx@gmail.com>
2026-01-17 10:17:26 -0600

    Add files via upload

---
+++ engine/editorState.js
@@ -0,0 +1,7 @@
+export const editorState = {
+  viewMode: 'topDown', // 'topDown' | 'angular'
+  camera: { x: 0, y: 0, zoom: 1 },
+  grid: { tileSize: 32, width: 0, height: 0 },
+  activeTool: 'paint',
+  dirty: false
+};
